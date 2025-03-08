4d05dbbd1984ea3299ca42d00f1c40b3f2b10a38
divakar-srinivasan <sdivakar2005@gmail.com>
2025-03-08 19:34:51 +0530

    8/3

--- src/components/child-components/about.jsx
+++ src/components/child-components/about.jsx
@@ -2,7 +2,8 @@ import bg1 from '../../assets/5.jpg'
 function About() {
   return (
     <div className="w-full h-screen bg-cover bg-center flex flex-col items-center justify-center" style={{ backgroundImage: `url(${bg1})` }}>
-      <h1 className="text-white text-3xl font-bold">About Page</h1>
+      <h1 className="text-white text-3xl font-bold">About dfghj;'
+        ' Page</h1>
     </div>
   );
 }
